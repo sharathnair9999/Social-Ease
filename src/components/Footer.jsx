6b9155c266ebafde1d6207b4727d58346cc8e990
Sharath <sharathnair9999@gmail.com>
2022-05-28 23:59:49 +0530

    Feat : Footer Component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,95 @@
 import React from "react";
+import { constants } from "../helpers";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="bg-slate-800 px-4 py-2 flex justify-center items-center gap-6 flex-col w-full">
+      <p className="md:text-xl text-md text-slate-300 text-center">
+        Created By{" "}
+        <a
+          href="https://sharath-nair9999.netlify.app/"
+          rel="noreferrer"
+          target="_blank"
+          className="hover:text-slate-50 transition duration-200 text-accent-1-light"
+        >
+          {`<Sharath/>`}
+        </a>
+      </p>
+      <section className="flex justify-center gap-4 items-center">
+        <p className="text-slate-200 md:text-xl text-md">Made using </p>
+        <section className="flex justify-center items-center gap-4">
+          <img
+            className="md:w-10 md:h-10 w-5 h-5"
+            src={constants.imgUrls.reactLogo}
+            alt="react"
+          />
+          <img
+            className="md:w-10 md:h-10 w-5 h-5"
+            src={constants.imgUrls.firebaseLogo}
+            alt="firebase"
+          />
+          <img
+            className="md:w-10 md:h-10 w-5 h-5"
+            src={constants.imgUrls.reduxLogo}
+            alt="reduxtoolkit"
+          />
+          <img
+            className="md:w-10 md:h-10 w-5 h-5"
+            src={constants.imgUrls.tailwindLogo}
+            alt="tailwindcss"
+          />
+        </section>
+      </section>
+
+      <section className="flex justify-center items-center gap-4">
+        <p className="md:text-xl text-md text-slate-300">Connect With Me</p>
+        <a
+          href="https://github.com/sharathnair9999"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={constants.imgUrls.githubLogo}
+            alt="github"
+            className="md:w-10 md:h-10 w-5 h-5"
+          />
+        </a>
+        <a
+          href="https://twitter.com/sharathnair9999"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={constants.imgUrls.twitterLogo}
+            alt="twitter"
+            className="md:w-10 md:h-10 w-5 h-5"
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/sharath99/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={constants.imgUrls.linkedinLogo}
+            alt="linkedin"
+            className="md:w-10 md:h-10 w-5 h-5"
+          />
+        </a>
+        <a
+          href="https://sharathcodes.hashnode.dev/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={constants.imgUrls.hashnodeLogo}
+            alt="hashnode"
+            className="md:w-10 md:h-10 w-5 h-5"
+          />
+        </a>
+      </section>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -4,5 +4,21 @@ export const constants = {
       "https://res.cloudinary.com/sharath-media-library/image/upload/v1653724138/social%20media/user-default_idyg7v.jpg",
     appLogo:
       "https://res.cloudinary.com/sharath-media-library/image/upload/v1653581502/social%20media/sm-logo_ukggxg.png",
+    reactLogo:
+      "https://res.cloudinary.com/sharath-media-library/image/upload/v1653754223/social%20media/icons8-react-native_srgtib.svg",
+    firebaseLogo:
+      "https://res.cloudinary.com/sharath-media-library/image/upload/v1653754223/social%20media/icons8-firebase-48_m64sqd.png",
+    reduxLogo:
+      "https://res.cloudinary.com/sharath-media-library/image/upload/v1653754223/social%20media/icons8-redux-48_whl4yx.png",
+    tailwindLogo:
+      "https://res.cloudinary.com/sharath-media-library/image/upload/v1653754223/social%20media/tailwind-css_mzocmr.svg",
+    hashnodeLogo:
+      "https://res.cloudinary.com/sharath-media-library/image/upload/v1653755115/social%20media/icons8-hashnode-48_mckcy7.png",
+    githubLogo:
+      "https://res.cloudinary.com/sharath-media-library/image/upload/v1653755115/social%20media/icons8-github-48_ov0ftw.png",
+    linkedinLogo:
+      "https://res.cloudinary.com/sharath-media-library/image/upload/v1653755115/social%20media/icons8-linkedin-2-48_smjbd0.png",
+    twitterLogo:
+      "https://res.cloudinary.com/sharath-media-library/image/upload/v1653755115/social%20media/icons8-twitter-48_ukzjgp.png",
   },
 };
