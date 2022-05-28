ee187f3591cd8222690211acdc4697d94c4533b4
Sharath <sharathnair9999@gmail.com>
2022-05-28 17:30:53 +0530

    Input Component

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,7 @@ import SearchBar from "./Searchbar/SearchBar";
 import SideNav from "./SideNav/SideNav";
 import TopNav from "./TopNav/TopNav";
 import FollowMore from "./FollowMore/FollowMore";
+import Brand from "./Brand";
+import Footer from "./Footer";
 
-export { SearchBar, SideNav, TopNav, FollowMore };
+export { SearchBar, SideNav, TopNav, FollowMore, Brand, Footer };
