00a2c0aef0b3ee533cf74ce6b7b2a5638c5ba12a
Sharath <sharathnair9999@gmail.com>
2022-05-29 18:28:12 +0530

    Auth handled using RTK

--- src/pages/MainContainer/MainContainer.jsx
+++ src/pages/MainContainer/MainContainer.jsx
@@ -3,11 +3,13 @@ import { FollowMore, SideNav, TopNav } from "../../components";
 
 const MainContainer = ({ children }) => {
   return (
-    <div>
+    <div className="min-h-screen">
       <TopNav />
-      <SideNav />
-      {children}
-      <FollowMore />
+      <div className="mt-14 flex justify-center items-start gap-4 pt-3">
+        <SideNav />
+        <div className="flex-grow">{children}</div>
+        <FollowMore />
+      </div>
     </div>
   );
 };
