2c3ea72303022cf3fcfc7f55e97e7138fd8a2a02
Sharath <sharathnair9999@gmail.com>
2022-05-29 18:22:59 +0530

    Top Nav Designed

--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logout } from "../../pages/Auth/helpers";
+import Brand from "../Brand";
+import Button from "./components/Button";
 
 const TopNav = () => {
-  return <div>TopNav</div>;
+  const navigate = useNavigate();
+
+  const authState = useSelector((state) => state.auth);
+
+  const [colorChange, setColorchange] = useState(false);
+  const changeNavbarColor = () => {
+    if (window.scrollY >= 50) {
+      setColorchange(true);
+    } else {
+      setColorchange(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeNavbarColor);
+
+  return (
+    <div
+      className={` ${
+        colorChange ? "bg-cta-light " : "bg-transparent"
+      } flex justify-start items-center p-2 fixed left-0 right-0 top-0 h-14 transition-all`}
+    >
+      <Brand full />
+      {authState.isLoggedIn && (
+        <Button
+          className={`ml-auto`}
+          onClick={() => {
+            logout();
+          }}
+        >
+          Logout
+        </Button>
+      )}
+      {!authState.isLoggedIn && (
+        <Button className={`ml-auto`} onClick={() => navigate("/login")}>
+          Login
+        </Button>
+      )}
+    </div>
+  );
 };
 
 export default TopNav;
