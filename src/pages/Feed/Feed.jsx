c26dbfb8bb4376f0b6013d60d19ae1d14b0712ca
Sharath <sharathnair9999@gmail.com>
2022-05-30 13:02:59 +0530

    Complete Feed UI

--- src/pages/Feed/Feed.jsx
+++ src/pages/Feed/Feed.jsx
@@ -1,18 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { Button, Modal, PostCard, PostEditor } from "../../components";
+import { useOnClickOutside } from "../../custom-hooks";
 
 const Feed = () => {
+  const { displayName, photoURL, uid } = useSelector((state) => state.auth);
+  const [showModal, setShowModal] = useState(false);
+  const ref = React.createRef();
+
   return (
     <div>
-      <div className="h-[20rem] w-full mb-4 bg-slate-400">content1</div>
-      <div className="h-[20rem] w-full mb-4 bg-slate-400">content</div>
-      <div className="h-[20rem] w-full mb-4 bg-slate-400">content</div>
-      <div className="h-[20rem] w-full mb-4 bg-slate-400">content</div>
-      <div className="h-[20rem] w-full mb-4 bg-slate-400">content</div>
-      <div className="h-[20rem] w-full mb-4 bg-slate-400">content</div>
-      <div className="h-[20rem] w-full mb-4 bg-slate-400">content</div>
-      <div className="h-[20rem] w-full mb-4 bg-slate-400">content</div>
-      <div className="h-[20rem] w-full mb-4 bg-slate-400">content</div>
-      <div className="h-[20rem] w-full mb-4 bg-slate-400">content</div>
+      <Modal showModal={showModal}>
+        <PostEditor ref={ref} newPost setShowModal={setShowModal} />
+      </Modal>
+      <div className=" shadow-md border-[1px] flex flex-col mb-4">
+        <section className="flex items-center justify-start gap-4 px-2 py-4  rounded-md">
+          <Link to={`/profile/${uid}`} className="max-w-[3rem]">
+            <img
+              src={photoURL}
+              alt={displayName}
+              className={`w-12 h-12 rounded-full`}
+            />
+          </Link>
+          <input
+            className="font-light border-none outline-none w-1/2"
+            value={"What's on your mind?"}
+            readOnly
+            onClick={() => setShowModal(true)}
+          />
+          <Button
+            onClick={() => setShowModal(true)}
+            className="bg-cta-dark text-white ml-auto "
+          >
+            New Post
+          </Button>
+        </section>
+      </div>
+
+      {[...Array(10)].map((_, id) => (
+        <PostCard
+          key={id}
+          postInfo={{
+            postDescription: `post${id + 1}`,
+            postId: `adslkjaskl${id + 1}`,
+          }}
+        />
+      ))}
     </div>
   );
 };
--- src/pages/MainContainer/MainContainer.jsx
+++ src/pages/MainContainer/MainContainer.jsx
@@ -3,10 +3,10 @@ import { FollowMore, SideNav, TopNav } from "../../components";
 
 const MainContainer = ({ children }) => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen flex justify-center items-start">
       <TopNav />
       <SideNav />
-      <div className=" md:max-w-5xl mt-16 px-4 md:min-w-[15rem] md:ml-[14rem] lg:mr-[14rem] ">
+      <div className=" lg:max-w-3xl md:max-w-4xl mt-16 px-2 md:min-w-[15rem] ">
         {children}
       </div>
       <FollowMore />
