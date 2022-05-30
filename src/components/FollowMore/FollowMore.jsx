ea5458a1cabcf4e37a8dde44ff34b6de22ad4731
Sharath <sharathnair9999@gmail.com>
2022-05-30 19:39:08 +0530

    Follow more component

--- src/components/FollowMore/FollowMore.jsx
+++ src/components/FollowMore/FollowMore.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import PersonCard from "../PersonCard";
 
 const FollowMore = () => {
   return (
-    <div className={`lg:block hidden md:fixed md:top-16 md:right-4`}>
-      FollowMore
+    <div
+      className={` lg:flex flex-col hidden md:fixed justify-start items-start gap-4 md:top-20 md:w-[16rem] lg:w-[17rem] md:right-4 h-96 overflow-y-auto  `}
+    >
+      {[...Array(20)].map((_, id) => (
+        <PersonCard key={id} id={id} />
+      ))}
     </div>
   );
 };
