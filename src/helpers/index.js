d51e493a76fd55a6d9bbdd605354794e67260993
Sharath <sharathnair9999@gmail.com>
2022-05-30 12:59:15 +0530

    Custom hook: Click outside handler

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,11 +3,13 @@ import RedirectLoggedInUser from "./RedirectLoggedInUser";
 import { handleChange } from "./handleChange";
 import { debounce } from "./debounce";
 import { constants } from "./constants";
+import { uploadFile } from "./uploadFile";
 
 export {
   ProtectedRoute,
   RedirectLoggedInUser,
   handleChange,
   debounce,
+  uploadFile,
   constants,
 };
