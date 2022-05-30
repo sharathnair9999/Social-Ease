fc92e00718c8c30e6d1de2220542dcc9ff14ac20
Sharath <sharathnair9999@gmail.com>
2022-05-30 19:50:57 +0530

    reduced post card size

--- src/pages/MainContainer/MainContainer.jsx
+++ src/pages/MainContainer/MainContainer.jsx
@@ -6,7 +6,7 @@ const MainContainer = ({ children }) => {
     <div className="min-h-screen flex justify-center items-start">
       <TopNav />
       <SideNav />
-      <div className=" lg:mr-[6rem] flex-grow xl:max-w-[45rem] md:max-w-[34rem] mt-16 px-2">
+      <div className=" lg:mr-[6rem] flex-grow xl:max-w-[40rem] md:max-w-[34rem] mt-16 px-2">
         {children}
       </div>
       <FollowMore />
