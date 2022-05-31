174bd94ca6cb27de17f27e2fa4a89ae03e0eef48
Sharath <sharathnair9999@gmail.com>
2022-05-31 20:37:55 +0530

    Explore page

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { PostCard } from "../../components";
+import { getAllPosts } from "../../features/Posts/postSlice";
+import { allPostsListener } from "../../services";
 
 const Explore = () => {
-  return <div>Explore</div>;
+  const { allPosts } = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    allPostsListener(dispatch, getAllPosts);
+  }, []);
+  return (
+    <div>
+      {allPosts?.map((post) => (
+        <PostCard key={post.postId} postInfo={post} />
+      ))}
+    </div>
+  );
 };
 
 export default Explore;
