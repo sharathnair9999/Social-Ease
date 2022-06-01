d84dda61576b78a8928b8658d47741ec87e429fc
Sharath <sharathnair9999@gmail.com>
2022-06-01 11:30:07 +0530

    Made protected route more secured

--- src/helpers/ProtectedRoute.jsx
+++ src/helpers/ProtectedRoute.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Navigate, useLocation } from "react-router-dom";
+import { Navigate, useLocation, useParams } from "react-router-dom";
 
 const ProtectedRoute = ({ children, route }) => {
   const userState = useSelector((state) => state.auth);
   const location = useLocation();
+  const { profileId } = useParams();
 
   return userState.isLoggedIn ? (
-    children
+    userState.uid !== profileId ? (
+      <Navigate replace={true} to={route} state={{ from: location }} />
+    ) : (
+      children
+    )
   ) : (
-    <Navigate replace={true} to={route} state={{ from: location }} />
+    <Navigate
+      replace={true}
+      to={`/profile/${profileId}`}
+      state={{ from: location }}
+    />
   );
 };
 
