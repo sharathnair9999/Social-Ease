0efc1dcf2192ddee75329a38f788d7e53bc7823b
Sharath <sharathnair9999@gmail.com>
2022-06-01 11:32:01 +0530

    Readable month year helper

--- src/helpers/readableDate.js
+++ src/helpers/readableDate.js
@@ -1,2 +1,21 @@
 export const getReadableDate = (timestamp) =>
   new Date(timestamp * 1000).toLocaleString();
+
+export const getMonthYear = (timeStamp) => {
+  const res = new Date(timeStamp * 1000);
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  return `${months[res.getMonth()]} ${res.getFullYear()}`;
+};
