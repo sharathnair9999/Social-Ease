5f66ed8b1c4440a7f56741add3512a0b0658da92
Sharath <sharathnair9999@gmail.com>
2022-06-08 00:10:04 +0530

    Reusable Person Card with follow/unfollow btn

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,9 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { followHandler } from "../services";
 import Button from "./Button";
 
 const PersonCard = ({ user }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { displayName, photoURL, uid, username } = user;
+  const auth = useSelector((state) => state.auth);
+  const {
+    loggedUser: { following },
+  } = useSelector((state) => state.user);
+  const isFollowing = following.some((person) => person === uid);
   return (
     <div className="flex justify-start items-center gap-2 w-full shadow-md p-1">
       <Link to={`/profile/${uid}`}>
@@ -14,16 +23,25 @@ const PersonCard = ({ user }) => {
         />
       </Link>
       <section className="flex justify-start items-start gap-0 flex-col">
-        <p className="text-md font-medium">{displayName}</p>
+        <Link to={`/profile/${uid}`}>
+          {" "}
+          <p className="text-md font-medium">{displayName}</p>
+        </Link>
         <p className="text-sm font-normal">{`@${username}`}</p>
       </section>
-      <Button
-        className={`ml-auto 
-        text-sm`}
-        // ${id % 2 === 0 ? "bg-cta-dark text-white" : " text-cta-dark"} will add this class back after controlling followers and following
-      >
-        {"Follow"}
-      </Button>
+      {auth.uid !== uid && (
+        <Button
+          onClick={() =>
+            auth.isLoggedIn
+              ? dispatch(followHandler({ personId: uid, user }))
+              : navigate("/login")
+          }
+          className={`ml-auto 
+        text-sm ${isFollowing ? "bg-cta-dark text-white" : " text-cta-dark"} `}
+        >
+          {`${isFollowing ? "Unfollow" : "Follow"}`}
+        </Button>
+      )}
     </div>
   );
 };
