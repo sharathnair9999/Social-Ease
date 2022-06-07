925d5a8608c36d8fc5c42fff9dadcc7593740902
Sharath <sharathnair9999@gmail.com>
2022-06-08 00:15:55 +0530

    Lazy loading Fallback screen

---
+++ src/pages/SplashScreen.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SplashScreen = () => {
+  return (
+    <div className="w-screen h-screen bg-gradient-to-br from-cta-dark/70 to-accent-1/70 flex justify-center items-center">
+      <p>Splash screen activity</p>
+    </div>
+  );
+};
+
+export default SplashScreen;
