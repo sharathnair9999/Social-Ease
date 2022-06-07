2e237a5416e0b59f0c18b7e1e9dfbf94bb131920
Sharath <sharathnair9999@gmail.com>
2022-06-08 00:14:45 +0530

    minor changes

--- src/pages/LikedPosts.jsx
+++ src/pages/LikedPosts.jsx
@@ -7,9 +7,9 @@ import { fetchUserLikedPosts } from "../services";
 const LikedPosts = () => {
   const { profileId } = useParams();
   const dispatch = useDispatch();
-  const { likedPosts, likedPostsLoading, likedPostsError } = useSelector(
-    (state) => state.user
-  );
+  const {
+    loggedUser: { likedPosts, likedPostsLoading, likedPostsError },
+  } = useSelector((state) => state.user);
   useEffect(() => {
     dispatch(fetchUserLikedPosts(profileId));
   }, [profileId, dispatch]);
@@ -19,8 +19,8 @@ const LikedPosts = () => {
       {likedPostsError && likedPostsError}
       {likedPostsLoading
         ? " Loading" // will add loading gif and error gif after completing the app
-        : likedPosts.length > 0
-        ? likedPosts.map((post) => (
+        : likedPosts?.length > 0
+        ? likedPosts?.map((post) => (
             <PostCard key={post.postId} postInfo={post} />
           ))
         : "You havent liked any posts till now"}
