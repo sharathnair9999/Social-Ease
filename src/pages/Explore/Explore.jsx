2d9f729d7e7a46345cf89245e98364a6f97c35f1
Sharath <sharathnair9999@gmail.com>
2022-06-08 21:10:46 +0530

    padding fix

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -18,7 +18,7 @@ const Explore = () => {
   }, [dispatch]);
   return (
     <div>
-      <div className="flex justify-end items-center gap-4 sticky bg-white w-full top-[3.25rem] p-1">
+      <div className="flex justify-end items-center gap-4 sticky bg-white w-full top-14 px-1 py-2">
         <button
           onClick={() => setSortBy("LATEST")}
           className={`px-2 py-1 rounded-md text-xs ${
